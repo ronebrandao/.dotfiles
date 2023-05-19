d025ee8d2b82315034223a4330b123e7bd920a19
Rone Brandao <rone_filho@hotmail.com>
2023-05-19 18:53:09 -0300

    Add additional keymaps and  remove sunmanko_lua config file

--- nvim/.config/nvim/lua/rone/remap.lua
+++ nvim/.config/nvim/lua/rone/remap.lua
@@ -13,3 +13,14 @@ map("n", "<c-l>", "<C-w>l")
 -- Telescope
 map("n", "<leader>f", ":Telescope find_files<CR>")
 map("n", "<c-t>", ":Telescope live_grep<CR>")
+
+--Text moving
+map("v", "J", ":m '>+1<CR>gv=gv")
+map("v", "K", ":m '<-2<CR>gv=gv")
+
+--Tab navigation
+map("n", "<leader>1", "1gt")
+map("n", "<leader>2", "2gt")
+map("n", "<leader>3", "3gt")
+map("n", "<leader>4", "4gt")
+map("n", "<leader>5", "5gt")
