878de444aef2a324a8cf3a4d3cb8ff7e7727c64c
Rone Brandao <rone_filho@hotmail.com>
2024-04-02 11:52:49 -0300

    new remaps

--- nvim/.config/nvim/lua/rone/remap.lua
+++ nvim/.config/nvim/lua/rone/remap.lua
@@ -15,3 +15,22 @@ map("n", "<c-t>", ":Telescope live_grep<CR>")
 --Text moving
 map("v", "J", ":m '>+1<CR>gv=gv")
 map("v", "K", ":m '<-2<CR>gv=gv")
+
+map("n", ":", ";")
+map("n", ";", ":")
+map("v", ":", ";")
+map("v", ";", ":")
+
+-- paste over the selected text without adding the deleted text to the register
+map("x", "<leader>p", [["_dP]])
+
+-- copies the selected text to the system clipboard.
+map("n", "<leader>y", [["+y]])
+map("v", "<leader>y", [["+y]])
+
+-- copies the entire current line to the system clipboard.
+map("n", "<leader>Y", [["+Y]])
+
+-- deletes text without adding to register
+map("n", "<leader>d", [["_d]])
+map("v", "<leader>d", [["_d]])
